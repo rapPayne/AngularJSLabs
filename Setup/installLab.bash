aa43aa534cd5046c191fbbf25cc984fac9617cff
Rap Payne <bob@creator.net>
2015-09-07 11:22:22 -0500

    Adding bash scripts

---
+++ Setup/installLab.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Before running this, you must have installed npm (with Node.js) and
+# MongoDB.
+
+# Need a connection to the Internet
+if ! (ping -c1 github.com >/dev/null)  ; then
+  echo "No connection to github. Please make sure you can connect to the Internet and try again." >&2
+  exit 1
+fi
+
+# Make sure npm is installed
+if (( npm --version >/dev/null 2>&1 )) ; then
+  echo "npm wasn't found. Make sure npm is installed and in your path and try again." >&2
+  exit 2
+fi
+
+# Create the Northwind Database
+./loadMongoCSVFiles.bash
+if (( $? != 0 )) ; then
+  exit 3
+fi
+
+# Run npm install for the Web Server
+cd "../WebServer"
+npm install
+
+# Run npm install for the app
+cd ../app
+npm install
+
+echo ""
+echo "Installation was successful."
+echo ""
