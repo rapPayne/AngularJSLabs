71c0f904318086ad3fe1b09085ea498e8eb56268
rapPayne <bob@creator.net>
2015-10-27 16:53:07 -0500

    Made the cart count in the pageHeader work

---
+++ app/shared/pageHeaderController.js
@@ -0,0 +1,18 @@
+(function () {
+  'use strict';
+  angular.module('sharedModule')
+    .controller('pageHeaderController',pageHeaderController);
+  pageHeaderController.$inject=['$scope', 'cartService'];
+  function pageHeaderController($scope, cartService) {
+    cartService.getCart().then(
+      function (response) {
+        console.log(response);
+        $scope.cart = response.data;
+      },
+      function (error) {
+        console.error("Error getting cart in pageHeader.", error);
+      }
+    );
+
+  }
+})();
