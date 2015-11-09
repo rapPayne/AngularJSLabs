e7e31ba49ed28a98c8622e84678a6abef8498fcf
rapPayne <bob@creator.net>
2015-11-09 14:31:13 -0600

    Removed commented out lines

--- app/shared/pageHeaderController.js
+++ app/shared/pageHeaderController.js
@@ -5,14 +5,5 @@
   pageHeaderController.$inject=['$scope', 'cartService'];
   function pageHeaderController($scope, cartService) {
     $scope.cart = cartService.cart;
-    //cartService.getCart();
-//      .then(
-//      function (response) {
-//        $scope.cart = response.data;
-//      },
-//      function (error) {
-//        console.error("Error getting cart in pageHeader.", error);
-//      }
-//    );
   }
 })();
