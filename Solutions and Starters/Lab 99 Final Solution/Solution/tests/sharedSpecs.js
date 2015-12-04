0cabbe0d707154deb0edbce6f9a7b6ca9caa93f1
rapPayne <bob@creator.net>
2015-12-04 14:44:55 -0600

    Added Jasmine and angular-mocks

---
+++ Solutions and Starters/Lab 99 Final Solution/Solution/tests/sharedSpecs.js
@@ -0,0 +1,67 @@
+/**
+ * Created by rap on 12/4/15.
+ */
+(function () {
+
+  describe("Shared Specs", function () {
+    beforeEach(angular.mock.module("sharedModule"));
+
+    describe("Filters can be tested", function () {
+      var coolerFilter;
+      beforeEach(function () {
+        angular.mock.inject(function (_coolerFilter_) {
+//          //coolerFilter = $injector.get('$filter')('coolerFilter');
+          console.log("Cooler filter is ",_coolerFilter_);
+        });
+      });
+      it('Should look even cooler', function () {
+        var oldText = "Batman";
+        var newText = "Batman is cooler now";
+        // = coolerFilter(oldText);
+        expect(newText).toBe('Batman is cooler now');
+      });
+    });
+
+    describe("Page Header Controller", function () {
+
+      beforeEach(angular.mock.inject(function(_$controller_) {
+        $controller = _$controller_;
+      }));
+
+      it("Should have stuff in the cart", function () {
+        var userService = {user: {firstName: "Rap", lastName: "Jones"}};
+        var cartService = {cart: [
+          {product: {productID: 1}, qty: 10}, {product: {productID: 2}, qty: 5}
+        ]};
+        var $scope = {user: userService.user, cart: cartService.cart};
+        var pageHeaderController = $controller('pageHeaderController', {$scope: $scope, userService: userService, cartService:cartService});
+        console.log("phc:",pageHeaderController);
+        expect(pageHeaderController).not.toBe('undefined');
+      });
+    });
+
+    describe("Notify Factory", function () {
+      var notifyFactory;
+
+      beforeEach(function () {
+        angular.mock.inject(function (_notifyFactory_) {
+          notifyFactory = _notifyFactory_;
+          //console.log(notifyFactory);
+        });
+      });
+
+      it("Should show something", function () {
+        var count = 0;
+        spyOn(notifyFactory, "showSuccess").and.callFake(function () {
+          count++;
+        });
+        notifyFactory.showSuccess("yo","title");
+        notifyFactory.showSuccess("yo","title");
+
+        expect(count).toEqual(2);
+      });
+
+    });
+  });
+
+})();
